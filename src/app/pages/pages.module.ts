f4d846dce00eb13492dc373e4fb3b2f574277c1a
AlexDev2808 <colores.2808.8000@gmail.com>
2023-05-30 13:00:05 -0600

    Usando atributos personalizados

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,7 +6,7 @@ import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
-
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -19,7 +19,8 @@ import { AppRoutingModule } from '../app-routing.module';
   imports: [
     CommonModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   exports: [
     DashboardComponent,
